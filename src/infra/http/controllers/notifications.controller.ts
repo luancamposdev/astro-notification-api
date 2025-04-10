954f2e3c782148bb1bf3bf2ced7ef2dc90539ea2
Luan Campos <luancamposdeveloper@gmail.com>
2025-04-10 00:35:40 -0300

    chore: initial config

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { NotificationDto } from '../dtos/notification.dto';
+import { SendNotification } from '../../../application/use-cases/send-notification';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
+  @Post()
+  async createNotification(@Body() data: NotificationDto) {
+    const { recipientId, content, category } = data;
+
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return { notification };
+  }
+}
