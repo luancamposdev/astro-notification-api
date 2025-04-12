1c12702ed79d771222d75a9ce5ab970c0c184a45
Luan Campos <luancamposdeveloper@gmail.com>
2025-04-11 22:17:07 -0300

    feat: create cancel notifications

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,34 @@
+import { CancelNotification } from './cancel-notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { NotificationNotFound } from '@application/use-cases/errors/notification-not-found';
+import { makeNotification } from '@test/factories/notification-factory';
+
+describe('Cancel notification', () => {
+  it('Should to be able to cancel notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = makeNotification();
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a notification when it does not exist', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    await expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'example-id-notification',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
